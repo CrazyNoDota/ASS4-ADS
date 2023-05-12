40088d47a0b2a5c48e2a0f153911d366b25bdf1e
Dzhalil <220963@astanait.edu.kz>
2023-05-12 13:38:10 +0600

    Fixing bug with contains

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ package src;
 public class Main {
     public static void main(String[] args){
         MyHashTable<String, Integer> table = new MyHashTable<String, Integer>();
-        table.put("Ask", 123);
+
         table.put("as", 1111);
-        System.out.println(table.get("as"));
+
+
+        System.out.println(table.contains(1111));
 
     }
 }
